e8ba6d1f373ac5ce9cfc4068bba517cfb9caea82
Matt Wingard <matt.wingard@orbitalsidekick.com>
2025-04-17 10:46:39 -0700

    adds a basic configuration class to the application

---
+++ app/api/core/config.py
@@ -0,0 +1,20 @@
+
+
+
+class AppConfig:
+    raster_data_folder:str
+
+
+
+def create_app_config() -> AppConfig:
+    """
+    Create the application configuration.
+    Returns:
+        AppConfig: The application configuration.
+    """
+    output_config = AppConfig()
+    output_config.raster_data_folder = "/home/matthew/Documents/projects/perennial-raster/app/raster/data"
+    return output_config
+
+
+app_config = create_app_config()
