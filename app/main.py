0fcfb379f50af6d62eda014a258a67ff96797d74
Matt Wingard <matt.wingard@orbitalsidekick.com>
2025-04-17 09:28:54 -0700

    creates a primary entrypoint into the application

---
+++ app/main.py
@@ -0,0 +1,20 @@
+"""
+Primary entry point to the flight log service
+"""
+from fastapi import FastAPI
+
+from app.api.routes.api import router as api_router
+
+
+
+def get_application() -> FastAPI:
+    """this function returns the fastapi application"""
+    application = FastAPI(
+        title="Sample Code for Raster Queries",
+    )
+
+    application.include_router(api_router)
+    return application
+
+
+app = get_application()
