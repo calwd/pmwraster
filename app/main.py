70f21a3ece580231b46d4b72693ee2c29c51a5d6
Matt Wingard <matt.wingard@orbitalsidekick.com>
2025-04-18 10:07:34 -0700

    adds a new config-environment title to the application

--- app/main.py
+++ app/main.py
@@ -4,12 +4,13 @@ Primary entry point to the application.
 from fastapi import FastAPI
 
 from app.api_routes import router as router
+from app.config import app_config
 
 
 def get_application() -> FastAPI:
     """this function returns the fastapi application"""
     application = FastAPI(
-        title="Sample Code for Raster Queries",
+        title=app_config.application_name,
     )
 
     application.include_router(router)
