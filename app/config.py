bb21ac56efc77c5de10bedb5df5eb7ccff5924e8
Matt Wingard <matt.wingard@orbitalsidekick.com>
2025-04-18 10:05:18 -0700

    addition of documentation to the config class and a new application_name attribute

--- app/config.py
+++ app/config.py
@@ -8,8 +8,16 @@ from pathlib import Path
 
 
 class AppConfig:
+    """
+    This class holds the application configuration settings as well as a logger instance. The included logger is
+    will need to be separated from this class in the future, but was included here for simplicity
+    Attributes:
+        raster_data_folder (Path): The path to the folder containing raster data.
+        logger (logging.Logger): The logger instance for the application.
+    """
     raster_data_folder: Path
     logger: logging.Logger
+    application_name: str
 
 
 def create_app_config() -> AppConfig:
@@ -26,16 +34,16 @@ def create_app_config() -> AppConfig:
 
     if base_app_directory is None:
         raise EnvironmentError("BASE_APP_DIRECTORY environment variable not set")
-
     raster_library_directory = os.path.join(base_app_directory, "raster_data")
-    output_config.raster_data_folder = Path(raster_library_directory)
 
     base_app_name = os.getenv("BASE_APP_NAME")
-
     if base_app_name is None:
         base_app_name = "Raster Query Application"
 
+    output_config.application_name = base_app_name
     output_config.logger = logging.getLogger(base_app_name)
+    output_config.raster_data_folder = Path(raster_library_directory)
+
 
     return output_config
 
